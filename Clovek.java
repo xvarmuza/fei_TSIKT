0a8179f23eff553f89f632a60d77fa42edf52a74
Tomique <varmuzatom@gmail.com>
2023-09-28 11:07:56 +0200

    test

--- Clovek.java
+++ Clovek.java
@@ -20,5 +20,32 @@ public class Clovek {
     public void setMeno(String meno){
         this.meno = meno;
     }
+
+
+    public void porovnajVek(){
+        if(this.vek < 40){
+            System.out.println("Si mladý");
+        }
+        else{
+            System.out.println("Si starý");
+       }
+    }
+
+    public void vypisCisla(int max){
+      /*    for(int i = 0; i < max; i++){
+            System.out.println(i);
+            if(i == 5 ){
+                continue;
+            }
+            System.out.println(i);
+        }
+        */
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+
 }
 
