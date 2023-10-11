bfc4d8242cf2a7f6e0e166a01a131de527adf592
Tomique <varmuzatom@gmail.com>
2023-10-11 19:29:20 +0200

    test

--- main.java
+++ main.java
@@ -1,23 +1,7 @@
 public class main {
 
     public static void main(String[] args) {
-/*
-        Clovek c1 = new Clovek("Fero",20,10);
-        Clovek c2 = new Clovek("Šimon",70,11);
-        System.out.println(c1.getVek());
-        c1.setVek(39);
-        System.out.println(c1.getVek());
 
-        System.out.println(c1.getMeno());
-        c1.setMeno("Jozko");
-        System.out.println(c1.getMeno());
-        c1.porovnajVek();
-        System.out.println(c2.getMeno());
-        c2.porovnajVek();
-
-        c1.vypisCisla(10);
-
-*/
 
 //primeNumber
         int cislo = 17;
